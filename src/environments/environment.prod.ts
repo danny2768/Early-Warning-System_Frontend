5ad5c28bc32cca3c39e8ebf22f4036dc4a0ee44e
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-05-29 02:52:22 -0500

    29/05/2024: envs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environments = {
+  baseUrl: 'https://early-warning-systembackend-production.up.railway.app',
+  mapbox_key: 'pk.eyJ1IjoiZGFubnkyNzY4IiwiYSI6ImNsd3JkNjhtYzBicmsybG9nZTlpNGdpN2gifQ.wT_GFZFlqu4rduMDBkuWRw'
+}
