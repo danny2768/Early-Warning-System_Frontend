0530019c4f26ea858a88b6f301ab04b9c500ed6c
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-05-29 02:03:34 -0500

    29/05/2024: stations table component

--- src/app/admin/components/stations-table/stations-table.component.ts
+++ src/app/admin/components/stations-table/stations-table.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { Subject, takeUntil } from 'rxjs';
+import { Station } from '../../../shared/interfaces/station.interface';
 
 @Component({
-  selector: 'app-stations-table',
+  selector: 'admin-stations-table',
   templateUrl: './stations-table.component.html',
   styleUrl: './stations-table.component.css'
 })
-export class StationsTableComponent {
+export class StationsTableComponent implements OnInit, OnDestroy {
+
+  private destroy$ = new Subject<void>();
+
+  public stations: Station[] = [];
+
+
+  constructor(
+    private adminService: AdminService,
+  ) {}
+
+
+  ngOnInit(): void {
+    this.getStations();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  getStations() {
+    this.adminService.getStations().pipe( takeUntil(this.destroy$) )
+      .subscribe({
+        next: (resp) => {
+          this.stations = resp.stations;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+  }
 
 }
