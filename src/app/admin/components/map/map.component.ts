7bf1336b521feaaaea08a2adbcd190dc5e7e3341
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-06-09 23:48:50 -0500

    09/06/2024: Loader & map style input

--- src/app/admin/components/map/map.component.ts
+++ src/app/admin/components/map/map.component.ts
@@ -1,14 +1,24 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { environments } from '../../../../environments/environment';
-import { GeolocateControl, LngLat, LngLatLike, Map, Marker } from 'mapbox-gl';
+import { LngLat, Map } from 'mapbox-gl';
+import { MapStyle } from '../../interfaces/map-style.type';
+import { setThrowInvalidWriteToSignalError } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'admin-map',
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent implements AfterViewInit, OnDestroy{
+export class MapComponent implements AfterViewInit, OnChanges {
+
+  @Input()
+  public style: MapStyle = 'standard';
+  private availableStyles: Record<string, string> = {
+    'standard': "mapbox://styles/mapbox/standard",
+    'streets': "mapbox://styles/mapbox/streets-v12",
+    'satellite': "mapbox://styles/mapbox/satellite-streets-v12"
+  };
 
   @ViewChild('map')
   public divMap?: ElementRef;
@@ -17,17 +27,25 @@ export class MapComponent implements AfterViewInit, OnDestroy{
   public zoom: number = 1.2;
   public currentLngLat: LngLat = new LngLat(-73.120454, 7.140268);
 
+  public isMapLoading: boolean = true;
+
   constructor(
     private adminService: AdminService,
   ) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['style'] && !changes['style'].isFirstChange()) {
+      this.updateMapStyle();
+    }
+  }
+
+  updateMapStyle(): void {
+    const newStyle = this.availableStyles[this.style];
+    this.map?.setStyle(newStyle);
+  }
 
   ngAfterViewInit(): void {
     this.buildMap();
-  }
-
-  ngOnDestroy(): void {
-
   }
 
   buildMap(): void {
@@ -36,11 +54,17 @@ export class MapComponent implements AfterViewInit, OnDestroy{
     this.map = new Map({
       accessToken: environments.mapbox_key,
       container: this.divMap.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: this.availableStyles[this.style], // style URL
       center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
       maxZoom: 19,
       minZoom: 1,
+    });
+
+    this.map.on('load', () => {
+      setTimeout(() => {
+        this.isMapLoading = false;
+      }, 1000);
     });
   }
 
